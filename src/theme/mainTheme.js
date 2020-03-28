98e7b7aa37e28d8241c449dbcf0dae5341a8c83b
Pawel L <virtuozoo@o2.pl>
2020-03-28 17:31:28 +0100

    first commit

---
+++ src/theme/mainTheme.js
@@ -0,0 +1,15 @@
+export const theme = {
+  primary: '#0593FB',
+  dark: '#3B3B4C',
+  light: 300,
+  normal: 400,
+  bold: 700,
+  fontSize: {
+    xxs: '1rem',
+    xs: '1.2rem',
+    s: '1.6rem',
+    m: '2.1rem',
+    l: '2.4rem',
+    xl: '4rem',
+  },
+};

53da9733243032257556803058ae5dad5a4fa154
Pawel L <virtuozoo@o2.pl>
2020-03-31 18:39:00 +0200

    change animation

--- src/animation/Transition.js
+++ src/animation/Transition.js
@@ -1,16 +1,15 @@
 export const Variants = {
   in: {
     opacity: 1,
-    scale: 1,
+    y: -10,
   },
   out: {
     opacity: 0,
-    scale: 0.8,
   },
 };
 
 export const Transition = {
   type: 'tween',
   ease: 'anticipate',
-  duration: 1,
+  duration: 1.5,
 };

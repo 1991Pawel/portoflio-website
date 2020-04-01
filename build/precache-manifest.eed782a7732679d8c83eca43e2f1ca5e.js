06facd276e1cd09c3e583efb6def8f9964f32536
1991Pawel <43860287+1991Pawel@users.noreply.github.com>
2020-04-01 12:33:58 +0200

    Add files via upload

---
+++ build/precache-manifest.eed782a7732679d8c83eca43e2f1ca5e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d96f3864fc50785f4f2b4975de51b36b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a143ac3cc039fbfdadd",
+    "url": "/static/js/2.94a40064.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.94a40064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d784b1ebcb9a607aba4",
+    "url": "/static/js/main.d7b9f005.chunk.js"
+  },
+  {
+    "revision": "39c676299a797d884af4",
+    "url": "/static/js/runtime-main.25b5471b.js"
+  },
+  {
+    "revision": "1a6b5ebab70583b03521bf3cb4c5d719",
+    "url": "/static/media/desktop-icon.1a6b5eba.svg"
+  },
+  {
+    "revision": "e41f997a1661d3538a25b774622f5430",
+    "url": "/static/media/emailicon.e41f997a.svg"
+  },
+  {
+    "revision": "ddc250127189a3b4ee4802e6c2493757",
+    "url": "/static/media/github.icon.ddc25012.svg"
+  },
+  {
+    "revision": "776eda91c85d5cf2b7a6c409d41a827e",
+    "url": "/static/media/mobilephone.776eda91.svg"
+  }
+]);
